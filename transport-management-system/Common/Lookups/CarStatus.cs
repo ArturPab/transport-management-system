55340242d28263c17757a8e63c86d7e5ae7c116e
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-06-06 18:11:53 +0200

    add sql builder

---
+++ transport-management-system/Common/Lookups/CarStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace transport_management_system.Common.Lookups
+{
+    public class CarStatusLookup
+    {
+        public static Dictionary<int, string> Descriptions = new()
+        {
+            {(int) CarStatus.Available, "Dostępny"},
+            {(int) CarStatus.Busy, "Zajęty"},
+            {(int) CarStatus.Broken, "Zepsuty"}
+        };
+    }
+    public enum CarStatus
+    {
+        Available,
+        Busy,
+        Broken,
+    }
+}
---
+++ transport-management-system/SqlBuilder/Enums/WhereOperators.cs
@@ -0,0 +1,12 @@
+﻿namespace transport_management_system.SqlBuilder.Enums
+{
+    public enum WhereOperators
+    {
+        Equal,
+        GreaterThan,
+        LessThan,
+        GreaterThanOrEqual,
+        LessThanOrEqual,
+        NotEqual,
+    }
+}
