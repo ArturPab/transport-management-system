6a8f4cef8e0b09cce3bfed07573c3d1c89043202
Artur Pabjan <artur.pabjan00@gmail.com>
2022-06-06 20:16:42 +0200

    TMS-2 created properties for db connection (checkpoint)

---
+++ transport-management-system/DB/DBConnectionService.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace transport_management_system.DB
+{
+    public class DBConnectionService
+    {
+        private readonly MySqlConnectionStringBuilder stringBuilder = new();
+
+        private static DBConnectionService? instance = null;
+        public static DBConnectionService Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DBConnectionService();
+                return instance;
+            }
+        }
+        public MySqlConnection Connection => new(stringBuilder.ToString());
+
+        private DBConnectionService()
+        {
+            stringBuilder.Server = Properties.Settings.Default.server;
+            stringBuilder.Database = Properties.Settings.Default.dbname;
+            stringBuilder.UserID = Properties.Settings.Default.username;
+            stringBuilder.Password = Properties.Settings.Default.password;
+            stringBuilder.Port = Properties.Settings.Default.port;
+        }
+    }
+}
---
+++ transport-management-system/SqlBuilder/SqlQueryResult.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using transport_management_system.DB;
+
+namespace transport_management_system.SqlBuilder
+{
+    public class SqlQueryResult
+    {
+        private readonly string query;
+
+        public SqlQueryResult(string query)
+        {
+            this.query = query;
+        }
+
+        public List<T> ExecuteQuery<T>() where T : new()
+        {
+            var results = new List<T>();
+            using (var connection = DBConnectionService.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    results.Add((T)Activator.CreateInstance(typeof(T), new object[] { reader }));
+                connection.Close();
+            }
+            return results;
+        }
+    }
+}
