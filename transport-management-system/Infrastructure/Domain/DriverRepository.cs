1ea1fd1b342adf34fe1ec847a52d1653d3faf885
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-06-24 15:42:10 +0200

    deriver repo

---
+++ transport-management-system/Infrastructure/Domain/DriverRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using transport_management_system.Infrastructure.SQL;
+using transport_management_system.Infrastructure.SQL.Enums;
+using transport_management_system.Infrastructure.SQL.QueryBuilders;
+using transport_management_system.MVVM.Model;
+
+namespace transport_management_system.Infrastructure.Domain
+{
+    public class DriverRepository
+    {
+        private const string TableName = "Driver";
+
+        #region SELECT
+
+        public IEnumerable<Driver> GetAllDrivers()
+        {
+            var builder = new MySqlSelectQueryBuilder();
+            var drivers = builder.SelectAllProperties<Driver>()
+                .From(TableName)
+                .Build()
+                .ExecuteQuery<Driver>();
+
+            return drivers;
+        }
+
+        public Driver GetDriver(int id)
+        {
+            var builder = new MySqlSelectQueryBuilder();
+            var driver = builder.SelectAllProperties<Driver>()
+                .From(TableName)
+                .Where("Id", WhereOperators.Equal, id)
+                .Build()
+                .ExecuteQuery<Driver>().FirstOrDefault();
+
+            if (driver == null)
+                throw new ArgumentException($"Driver with id: {id} doesn't exist");
+
+            return driver;
+        }
+
+        #endregion
+
+        #region INSERT
+
+        public int AddDriver(Driver driver)
+        {
+            var id = (int) new MySqlInsertQuery<Driver>("Driver", driver).ExecuteQuery();
+
+            driver.Id = id;
+
+            return id;
+        }
+
+        #endregion
+
+        #region UPDATE
+
+        public void UpdateDriver(Driver driver)
+        {
+            new MySqlUpdateQueryBuilder<Driver>(driver).Build().ExecuteQuery();
+        }
+
+        #endregion
+
+        #region DELETE
+
+        public void RemoveDriver(int id)
+        {
+            new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, id);
+
+        }
+
+        public void RemoveDriver(Driver driver)
+        {
+            if (driver.Id == null)
+                throw new ArgumentException("Driver has no Id");
+
+            new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, driver.Id);
+        }
+
+        #endregion
+    }
+}
