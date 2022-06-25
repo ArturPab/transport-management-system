426308212779100feee71b309605d3ed8a3e02ec
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-06-25 22:45:07 +0200

    add repos, model ctors

---
+++ transport-management-system/Infrastructure/Domain/CompanyRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using transport_management_system.Domain;
+using transport_management_system.Infrastructure.SQL;
+using transport_management_system.Infrastructure.SQL.Enums;
+using transport_management_system.Infrastructure.SQL.QueryBuilders;
+
+namespace transport_management_system.Infrastructure.Domain
+{
+    public class CompanyRepository
+    {
+        private const string TableName = "Company";
+
+        #region SELECT
+
+        public IEnumerable<Company> GetAllCompanies()
+        {
+            var builder = new MySqlSelectQueryBuilder();
+            var companys = builder.SelectAllProperties<Company>()
+                .From(TableName)
+                .Build()
+                .ExecuteQuery<Company>();
+
+            return companys;
+        }
+
+        public Company GetCompany(int id)
+        {
+            var builder = new MySqlSelectQueryBuilder();
+            var company = builder.SelectAllProperties<Company>()
+                .From(TableName)
+                .Where("Id", WhereOperators.Equal, id)
+                .Build()
+                .ExecuteQuery<Company>().FirstOrDefault();
+
+            if (company == null)
+                throw new ArgumentException($"Company with id: {id} doesn't exist");
+
+            return company;
+        }
+
+        #endregion
+
+        #region INSERT
+
+        public int AddCompany(Company company)
+        {
+            var id = (int) new MySqlInsertQuery<Company>("Company", company).ExecuteQuery();
+
+            company.Id = id;
+
+            return id;
+        }
+
+        #endregion
+
+        #region UPDATE
+
+        public void UpdateCompany(Company company)
+        {
+            new MySqlUpdateQueryBuilder<Company>(company).Build().ExecuteQuery();
+        }
+
+        #endregion
+
+        #region DELETE
+
+        public void RemoveCompany(int id)
+        {
+            new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, id);
+
+        }
+
+        public void RemoveCompany(Company company)
+        {
+            if (company.Id == null)
+                throw new ArgumentException("Company has no Id");
+
+            new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, company.Id);
+        }
+
+        #endregion
+    }
+}
---
+++ transport-management-system/Infrastructure/Domain/OrderRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using transport_management_system.Infrastructure.SQL;
+using transport_management_system.Infrastructure.SQL.Enums;
+using transport_management_system.Infrastructure.SQL.QueryBuilders;
+using transport_management_system.MVVM.Model;
+
+namespace transport_management_system.Infrastructure.Domain
+{
+    public class OrderRepository
+    {
+        private const string TableName = "Order";
+
+        #region SELECT
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            var builder = new MySqlSelectQueryBuilder();
+            var orders = builder.SelectAllProperties<Order>()
+                .From(TableName)
+                .Build()
+                .ExecuteQuery<Order>();
+
+            return orders;
+        }
+
+        public Order GetOrder(int id)
+        {
+            var builder = new MySqlSelectQueryBuilder();
+            var order = builder.SelectAllProperties<Order>()
+                .From(TableName)
+                .Where("Id", WhereOperators.Equal, id)
+                .Build()
+                .ExecuteQuery<Order>().FirstOrDefault();
+
+            if (order == null)
+                throw new ArgumentException($"Order with id: {id} doesn't exist");
+
+            return order;
+        }
+
+        #endregion
+
+        #region INSERT
+
+        public int AddOrder(Order order)
+        {
+            var id = (int) new MySqlInsertQuery<Order>("Order", order).ExecuteQuery();
+
+            order.Id = id;
+
+            return id;
+        }
+
+        #endregion
+
+        #region UPDATE
+
+        public void UpdateOrder(Order order)
+        {
+            new MySqlUpdateQueryBuilder<Order>(order).Build().ExecuteQuery();
+        }
+
+        #endregion
+
+        #region DELETE
+
+        public void RemoveOrder(int id)
+        {
+            new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, id);
+
+        }
+
+        public void RemoveOrder(Order order)
+        {
+            if (order.Id == null)
+                throw new ArgumentException("Order has no Id");
+
+            new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, order.Id);
+        }
+
+        #endregion
+    }
+}
