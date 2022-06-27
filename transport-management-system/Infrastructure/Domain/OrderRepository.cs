29f0ab360a46b182100ec817fbc9338f55aa0e11
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-06-27 12:03:39 +0200

    assigning

--- transport-management-system/Infrastructure/Domain/OrderRepository.cs
+++ transport-management-system/Infrastructure/Domain/OrderRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using transport_management_system.Domain;
 using transport_management_system.Infrastructure.SQL;
 using transport_management_system.Infrastructure.SQL.Enums;
 using transport_management_system.Infrastructure.SQL.QueryBuilders;
@@ -24,6 +26,12 @@ namespace transport_management_system.Infrastructure.Domain
                 .Build()
                 .ExecuteQuery<Order>();
 
+            foreach(var order in orders)
+            {
+                order.Company = CompanyRepository.Instance.GetCompany(order.CompanyId.Value);
+                order.Route = RouteRepository.Instance.GetRoute(order.RouteId.Value);
+            }
+
             return orders;
         }
 
@@ -38,6 +46,9 @@ namespace transport_management_system.Infrastructure.Domain
 
             if (order == null)
                 throw new ArgumentException($"Order with id: {id} doesn't exist");
+
+            order.Company = CompanyRepository.Instance.GetCompany(order.CompanyId.Value);
+            order.Route = RouteRepository.Instance.GetRoute(order.RouteId.Value);
 
             return order;
         }
@@ -80,6 +91,56 @@ namespace transport_management_system.Infrastructure.Domain
                 throw new ArgumentException("Order has no Id");
 
             new MySqlDeleteQueryBuilder().From(TableName).Where("Id", WhereOperators.Equal, order.Id).Build().ExecuteQuery();
+        }
+
+        #endregion
+
+        #region Assigning
+
+        public void AssignDriversToOrder(Order order, List<Driver> drivers)
+        {
+            new MySqlDeleteQueryBuilder().From("performs").Where("OrderId", WhereOperators.Equal, order.Id.Value).Build().ExecuteQuery();
+
+            foreach (var driver in drivers)
+            {
+                new MySqlInsertQuery<Performs>("performs", new Performs(order.Id.Value, driver.Id.Value)).ExecuteQuery();
+            }
+        }
+
+        public List<Driver> GetAssignDriversForOrder(Order order)
+        {
+            var performs = new MySqlSelectQueryBuilder().SelectAllProperties<Performs>()
+                .From("performs")
+                .Where("OrderId", WhereOperators.Equal, order.Id.Value)
+                .Build()
+                .ExecuteQuery<Performs>();
+
+            var drivers = performs.Select(performs => DriverRepository.Instance.GetDriver(performs.DriverId)).ToList();
+
+            return drivers;
+        }
+
+        public void AssignCarsToOrder(Order order, List<Car> cars)
+        {
+            new MySqlDeleteQueryBuilder().From("realizes").Where("OrderId", WhereOperators.Equal, order.Id.Value).Build().ExecuteQuery();
+
+            foreach (var car in cars)
+            {
+                new MySqlInsertQuery<Realizes>("realizes", new Realizes(order.Id.Value, car.Id.Value)).ExecuteQuery();
+            }
+        }
+
+        public List<Car> GetAssignCarsForOrder(Order order)
+        {
+            var realizes = new MySqlSelectQueryBuilder().SelectAllProperties<Realizes>()
+                .From("realizes")
+                .Where("OrderId", WhereOperators.Equal, order.Id.Value)
+                .Build()
+                .ExecuteQuery<Realizes>();
+
+            var cars = realizes.Select(realize => CarRepository.Instance.GetCar(realize.CarId)).ToList();
+
+            return cars;
         }
 
         #endregion
